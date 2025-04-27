00906ee45df623dacd290fc35419b7821e397105
1zeroseven4 <zerosevensign@gmail.com>
2025-04-28 01:04:23 +0800

    config: inherit mind the gapps
    Signed-off-by: 1zeroseven4 <zerosevensign@gmail.com>

--- config/features.mk
+++ config/features.mk
@@ -45,8 +45,7 @@ endif
 
 # GAPPS
 ifeq ($(TARGET_BUILD_PACKAGE),3)
-  BUILD_GMS_OVERLAYS_AND_PROPS := true
-  $(call inherit-product, vendor/gms/products/gms.mk)
+ $(call inherit-product, vendor/gapps/gapps.mk)
 else
   ifeq ($(TARGET_BUILD_PACKAGE),2)
     $(call inherit-product, vendor/microg/product.mk)
